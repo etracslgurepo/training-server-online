ed36ecdded1cbaa8130068687804f14941057c62
worgflores <worgflores@gmail.com>
2023-08-11 12:57:12 +0800

    added patch script

---
+++ _res/patches/01-patch-etracs-2023-08-11.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO `sys_report_template` (`name`, `title`, `filepath`, `master`, `icon`) 
+VALUES ('bpls/uaf/main-new', 'Unified Business Application Form', 'bpls/uaf/main-new.jasper', '0', NULL);
