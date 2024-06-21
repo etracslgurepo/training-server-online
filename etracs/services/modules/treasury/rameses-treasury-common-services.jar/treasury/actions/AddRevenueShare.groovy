9321bbe3a471028bdf12114da505dd037a713457
worgflores <worgflores@gmail.com>
2024-06-21 14:30:59 +0800

    treasury: updated

--- etracs/services/modules/treasury/rameses-treasury-common-services.jar/treasury/actions/AddRevenueShare.groovy
+++ etracs/services/modules/treasury/rameses-treasury-common-services.jar/treasury/actions/AddRevenueShare.groovy
@@ -12,36 +12,59 @@ class AddRevenueShare implements RuleActionHandler  {
 
 	public void execute(def params, def drools) {
 
-		def refitem = params.refitem;
-		def payableaccount = params.payableaccount;
 		def amt = params.amount.decimalValue;
+		if ( amt == 0 ) {
+			// do not proceed if value is ZERO 
+			return; 
+		}
+
+		def payableaccount = params.payableaccount;
+		def refitem = params.refitem;
 		def org = params.org;
 
-		if( refitem == null )
-			throw new Exception("refitem is required in AddRevenueShare action. Please indicate a ref item. Check the rule " + drools.rule.name );
-
-		if( payableaccount?.key == null || payableaccount?.key == 'null' )
-			throw new Exception("Payable account or generic payable account is required in AddRevenueShare action. Check the rule " + drools.rule.name);
+		if ( refitem == null )
+			throw new Exception("Ref Item is required in AddRevenueShare action. Check the rule " + drools.rule.name );
+		if ( !refitem.account?.objid )
+			throw new Exception("Ref Item must have an account in AddRevenueShare action. Check the rule " + drools.rule.name );
 
 		def ct = RuleExecutionContext.getCurrentContext();
+		def refItemAcct = ct.env.acctUtil.createAccountFact([ objid: refitem.account.objid ]); 
+		if ( !refItemAcct?.objid ) { 
+			throw new Exception("Set a valid Ref Item account in AddRevenueShare action. Check the rule " + drools.rule.name);	
+		}
+
+		def payItemAcct = null; 
+		if ( org?.orgid ) { 
+			payItemAcct = ct.env.acctUtil.createAccountFactByOrg( refItemAcct.objid, org.orgid ); 
+			if ( !payItemAcct?.objid ) {
+				throw new Exception("No available payable account for org "+ org.orgid + " in AddRevenueShare action. Check the rule " + drools.rule.name);	
+			}
+		} 
+
+		if ( payableaccount?.key && payableaccount?.key != 'null' ) {
+			payItemAcct = ct.env.acctUtil.createAccountFact([ objid: payableaccount.key ]);	
+			if ( !payItemAcct?.objid ) {
+				throw new Exception("No available payable account for org "+ org.orgid + " in AddRevenueShare action. Check the rule " + drools.rule.name);	
+			}
+		}
+
+		if ( payItemAcct?.objid ) {
+			// everything is set 
+		}
+		else { 
+			throw new Exception("Payable account or generic payable account is required in AddRevenueShare action. Check the rule " + drools.rule.name);
+		} 
+
 		def rs = new RevenueShare();
 		rs.receiptitemid = refitem.refid;
-		
-		rs.refitem = ct.env.acctUtil.createAccountFact( [objid: refitem.account.objid] );
-
-		if( org == null ) {
-			rs.payableitem = ct.env.acctUtil.createAccountFact( [objid: payableaccount.key] );	
-		}
-		else {
-			rs.payableitem = ct.env.acctUtil.createAccountFactByOrg( payableaccount.key, org.orgid ); 
-			if ( !rs.payableitem ) throw new Exception('There is no payable account with parent '+ payableaccount.value + ' org '+ org?.orgid);
-		}
-
+		rs.refitem = refItemAcct; 
+		rs.payableitem = payItemAcct; 
 		rs.amount  = amt;
+
 		if (!ct.result.sharing) {
 			ct.result.sharing = []
 		}
+
 		ct.facts << rs;
 	}
-
 }
